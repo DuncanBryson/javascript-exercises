00b5ac263cafd2c00d219945b003bfa6d6a4819b
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-01 11:09:12 -0700

    made and tested removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+  let array = arguments[0];
+  // array.splice(array.indexOf(arguments[1]), 1);
+  
+  
+  for(let i = 1; i < arguments.length; i++){
+    while (array.includes(arguments[i])){
+      array.splice(array.indexOf(arguments[i]),1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
