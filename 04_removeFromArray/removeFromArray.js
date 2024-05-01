a76c04185764beb94ad36e90a021e2b1e5050459
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-01 11:14:27 -0700

    removed comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,5 @@
 const removeFromArray = function() {
   let array = arguments[0];
-  // array.splice(array.indexOf(arguments[1]), 1);
-  
-  
   for(let i = 1; i < arguments.length; i++){
     while (array.includes(arguments[i])){
       array.splice(array.indexOf(arguments[i]),1);
