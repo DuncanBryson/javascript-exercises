2bc70715f53ca2d9d8470a3d841859f57a784afd
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-01 13:37:43 -0700

    Calculator working but needs improvement

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  for (const number of numbers) sum += number;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  for (const number of numbers) total *= number;
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+	let answer = 1;
+  for (let i = factorial; i>1; i--) answer *=i;
+  return answer;
 };
 
 // Do not edit below this line
