56f008f453204b7b4a4c369c1fe6b325f557a732
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-01 11:43:56 -0700

    made and tested sumAll with error catcher

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(intOne,intTwo) {
+  let sum = 0;
+  if (errorCatcher(intOne,intTwo)) return "ERROR";
+  let lowerBound = intOne,
+   upperBound = intTwo;
+  if (intOne > intTwo) lowerBound = intTwo, upperBound = intOne;
+  for (let i = lowerBound; i <= upperBound; i++){
+    sum += i;
+  }
+  return sum;
 };
+
+function errorCatcher(intOne, intTwo){
+  if (intOne < 0 || intTwo < 0) return true;
+  else if (typeof intOne != 'number' || typeof intTwo != 'number') return true;
+  else return false;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
