0804b5a9f130b36d49c4f31e213df95884bef9f2
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-05 13:51:26 -0700

    switched to array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,14 @@
+const alphanumeric = "qwertyuiopasdfghjklzxcvbnm1234567890";
+
 const palindromes = function (test) {
-  let reverse = '';
-  test = test
-    .replace(/[!,. ]/g, "")
-    .toLowerCase();
-  for (i = 0; i<test.length; i++){
-    reverse += test.charAt(test.length - i -1);
-  }
-  if (reverse == test) return true;
-  else return false;
-};
+ test = test
+  .toLowerCase()
+  .split("")
+  .filter ((char) => alphanumeric.includes(char))
+  .join("");
+const comparison = test.split("").reverse().join("");
+return test === comparison;
+
+}
 // Do not edit below this line
 module.exports = palindromes;
