01f2015dae8bda69d16039d3dbb7eb4cfbcf9fbb
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-05 14:52:24 -0700

    added age sort function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+// sort array by age. return oldest
+const findTheOldest = function(array) {
+  array.sort((a,b) => age(a) > age(b) ? -1:1);
+  console.table(array);
+  return array[0];
 };
+
+// get age of person in array
+function age(obj) {
+  return (obj.yearOfDeath || 2024) - obj.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
