551053233c226801a122dd469a16a6658d5cbb85
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-05 15:01:35 -0700

    switched to reduce from filter

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,14 @@
 // sort array by age. return oldest
 const findTheOldest = function(array) {
-  array.sort((a,b) => age(a) > age(b) ? -1:1);
-  console.table(array);
-  return array[0];
+  return array.reduce((a,b) => getAge(a) > getAge(b) ? a:b);
 };
 
-// get age of person in array
-function age(obj) {
-  return (obj.yearOfDeath || 2024) - obj.yearOfBirth;
+// get age of person
+function getAge(obj) {
+  return (obj.yearOfDeath || currentYear) - obj.yearOfBirth;
 }
+
+const currentYear = new Date().getFullYear();
 
 // Do not edit below this line
 module.exports = findTheOldest;
