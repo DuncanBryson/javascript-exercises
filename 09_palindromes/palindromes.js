a84dc0728e34a0ec2a37108ed090a542303447d4
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-05 13:34:17 -0700

    made and tested palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (test) {
+  let reverse = '';
+  test = test
+    .replace(/[!,. ]/g, "")
+    .toLowerCase();
+  for (i = 0; i<test.length; i++){
+    reverse += test.charAt(test.length - i -1);
+  }
+  if (reverse == test) return true;
+  else return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
